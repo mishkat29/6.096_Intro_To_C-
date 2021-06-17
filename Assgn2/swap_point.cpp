f61051fa64063e5a75d216831ba8240ccbc0927b
Mishkat Ahmad Neyazi <49838423+mishkat29@users.noreply.github.com>
2021-06-18 00:40:33 +0530

    Add files via upload

---
+++ Assgn2/swap_point.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void swap(int **a, int **b){
+    int temp =**a;
+    **a = **b;
+    **b= temp;
+}
+int main(){
+    int a =7;
+    int b =8;
+    int *x=&a;
+    int *y=&b;
+    swap(&x,&y);
+    cout << *x <<" "<<*y;
+}
